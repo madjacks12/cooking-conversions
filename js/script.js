45930c6eab5e31c90941ef3da14f6fb1c5461030
Amelia Jacobs and Marissa Chaney <amelia@>
2017-10-04 20:05:58 -0700

    Add further functions to handle subtraction, multiplication, and division

--- js/script.js
+++ js/script.js
@@ -2,7 +2,21 @@ var add = function(num1, num2) {
   return num1 + num2;
 };
 
+var subtract = function(num1, num2) {
+  return num1 - num2;
+};
+
+var multiply = function(num1, num2) {
+  return num1 * num2;
+};
+
+var divide = function(num1, num2) {
+  return num1 / num2;
+};
+
 var num1 = parseInt (prompt("Enter a number:"));
 var num2 = parseInt (prompt("Enter another number:"));
 
-alert(add(num1,num2));
+
+
+alert(divide(num1,num2));
